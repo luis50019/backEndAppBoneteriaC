a18feb302b36ba6d2ac0c67df1e5ef84a2142267
LuisDiaz <teddiazdiaz019@gmail.com>
2024-12-20 23:24:24 -0600

    cambiando ajustes de cors

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ import { routerSales } from "./Router/Sales.js";
 import { corsMiddleware } from "./Middleware/cors.js";
 import { routerStadisctic } from "./Router/stadistic.js";
 import { connectDB } from "./Model/mongoDB/dbMongo.js";
-
+import cors from 'cors'
 const PORT = process.PORT || 4000;
 const app = express();
 connectDB()
-app.use(corsMiddleware());
+app.use(cors({
+  origin: "*"
+}));
 app.use(json());
 app.use("/products", routerProducts);
 app.use("/sales", routerSales);
