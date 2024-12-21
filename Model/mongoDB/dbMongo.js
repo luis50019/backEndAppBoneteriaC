697afc2fc80358435a2df8219b4ca766cb5f0fed
LuisDiaz <teddiazdiaz019@gmail.com>
2024-12-20 23:00:49 -0600

    conectandose ala base en la nube

--- Model/mongoDB/dbMongo.js
+++ Model/mongoDB/dbMongo.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async()=>{
   try {
-    await mongoose.connect("mongodb://localhost:27017/InvenatarioBoneteriaSofy");
+    await mongoose.connect("mongodb+srv://teddiazdiaz019:GUlaz0ozhsm4Ih7S@cluster0.tmn1r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log(">>>> DB is connect")
   } catch (error) {
     console.log(error)
