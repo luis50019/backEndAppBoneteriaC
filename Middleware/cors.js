b4b5389e871e0548cd61222d8b25c8b0a6b48cec
LuisDiaz <teddiazdiaz019@gmail.com>
2024-12-20 23:08:23 -0600

    cambiando url

--- Middleware/cors.js
+++ Middleware/cors.js
@@ -2,7 +2,7 @@
 
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = ["https://inventario-boneteria.onrender.com","https://inventario-boneteria-2lq5q3tgl-luis-projects-7fa9a5dd.vercel.app","http://192.168.1.75:5173","http://localhost:5173"];
+const ACCEPTED_ORIGINS = ["https://inventario-boneteria.onrender.com","https://inventario-boneteria.onrender.com/","https://inventario-boneteria-2lq5q3tgl-luis-projects-7fa9a5dd.vercel.app","http://192.168.1.75:5173","http://localhost:5173"];
 
 export const corsMiddleware =({accepted_origins = ACCEPTED_ORIGINS} ={})=>{
   return cors({
