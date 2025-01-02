ed37c038889befb818953e98dfb00c62c65a4b26
LuisDiaz <teddiazdiaz019@gmail.com>
2025-01-02 02:27:48 -0600

    corrigiendo problemas de validacion

--- Schema/products.js
+++ Schema/products.js
@@ -15,7 +15,7 @@ const SchemaProducts = z.object({
   imageUrl: z.array(z.string({message:"No cumple con con el requisito de un enlacee"}),{message
 
   : "La imagen debe ser un enlace"
-  }),
+  }).optional(),
   purchasePrice: z
     .number({ invalid_type_error: "El precio de compra debe ser un número" })
     .positive({ message: "El precio de compra no es válido" }),
